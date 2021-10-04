0b21341af9ad87424698f18352be03ac77f3aabf
Felix Green <work@felix.green>
2021-10-04 15:41:33 -0400

    stub the tests

--- test/TheTapestry.ts
+++ test/TheTapestry.ts
@@ -26,7 +26,35 @@ describe("TheTapestry", () => {
   })
 
   // 4
-  describe("count up", async () => {
-    it("should count up", async () => {})
+  describe("weaving", async () => {
+    it("anyone should be able to add a line to the tapestry", async () => {
+      //
+    })
+
+    it("anyone CANT add more than 1 line to the tapestry", async () => {
+      // TODO: confirm whether this is per tapestry or chapter
+    })
+
+    // chaptering
+
+    // describe('chapters', async () => {
+    //   //
+    // })
+    // describe('lines', async () => {
+    //   //
+    // })
+
+    describe("content constraints", async () => {
+      it("lines are limited to 16 words", async () => {
+        // TODO: maybe characters makes more sense?
+        // is this valid?
+        // "a b c d e f g h i j k"
+        // "thisisaverylongnotwords but now we think itisaword"
+      })
+
+      it("stanzas are limited to 16 lines", async () => {
+        //
+      })
+    })
   })
 })
