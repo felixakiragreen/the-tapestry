855f885bb926cc3ab1c9ba055813125c549930c8
Kurt Schroeder <kurt@sators.com>
2021-10-13 13:23:06 -0400

    Only allow one line per wallet

--- contracts/TheTapestry.sol
+++ contracts/TheTapestry.sol
@@ -1,28 +1,34 @@
 // SPDX-License-Identifier: UNLICENSE
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract TheTapestry is ERC721 {
-    uint256 currentLine;
-    mapping(uint256 => string) public tapestryLines;
+	uint256 currentLine;
+	mapping(uint256 => string) public tapestryLines;
 
-    constructor() ERC721("TheTapestry", "TAPESTRY") {}
+	constructor() ERC721('TheTapestry', 'TAPESTRY') {}
 
-    // anyone should be able to add a line to the tapestry
-    function weave(string memory _line) public {
-        // require(_line.length > 0, "Line must be non-empty");
-        require(
-            bytes(_line).length < 101,
-            "Lines are limited to 100 characters"
-        );
+	// anyone should be able to add a line to the tapestry
+	function weave(string memory _line) public {
+		// require(_line.length > 0, "Line must be non-empty");
+		require(
+			bytes(_line).length < 101,
+			'Lines are limited to 100 characters'
+		);
 
-        // start with line 1
-        currentLine++;
-        tapestryLines[currentLine] = _line;
+		// require number of lines in author wallet to be less than 1
+		require(
+			balanceOf(msg.sender) < 1,
+			'Authors CANT add more than 1 line to the tapestry'
+		);
 
-        _safeMint(msg.sender, currentLine);
-    }
+		// start with line 1
+		currentLine++;
+		tapestryLines[currentLine] = _line;
+
+		_safeMint(msg.sender, currentLine);
+	}
 }
